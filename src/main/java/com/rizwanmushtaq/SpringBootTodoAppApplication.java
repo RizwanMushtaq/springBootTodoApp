a09b715fca39f193a676accf349c212bc69cfc5e
Rizwan <rizwanmushtaq15@gmail.com>
2025-07-01 17:16:43 +0200

    initial commit

---
+++ src/main/java/com/rizwanmushtaq/SpringBootTodoAppApplication.java
@@ -0,0 +1,11 @@
+package com.rizwanmushtaq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTodoAppApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootTodoAppApplication.class, args);
+  }
+}
