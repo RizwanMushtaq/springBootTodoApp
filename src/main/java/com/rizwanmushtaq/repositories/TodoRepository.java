dfc16402f1304c22713ccdede307b458e0caa116
Rizwan <rizwanmushtaq15@gmail.com>
2025-07-02 16:42:17 +0200

    removed extra logger

--- src/main/java/com/rizwanmushtaq/repositories/TodoRepository.java
+++ src/main/java/com/rizwanmushtaq/repositories/TodoRepository.java
@@ -1,15 +1,12 @@
 package com.rizwanmushtaq.repositories;
 
 import com.rizwanmushtaq.models.Todo;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class TodoRepository {
   private static List<Todo> todos = new ArrayList<>();
-  private static Logger logger = LoggerFactory.getLogger(TodoRepository.class);
 
   public static List<Todo> getTodos() {
     return todos;
@@ -26,7 +23,6 @@ public abstract class TodoRepository {
 
   public static Todo createTodo(Todo todo) {
     todos.add(todo);
-    logger.info("updated Todo List" + todos);
     return todo;
   }
 
