02a155725ed69d95a6721b39cf60d45902db1b9d
Rizwan <rizwanmushtaq15@gmail.com>
2025-07-18 16:56:50 +0200

    added mysql-connector

--- src/main/java/com/rizwanmushtaq/SpringBootTodoAppApplication.java
+++ src/main/java/com/rizwanmushtaq/SpringBootTodoAppApplication.java
@@ -1,10 +1,15 @@
 package com.rizwanmushtaq;
 
+import com.rizwanmushtaq.dao.TodoDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootTodoAppApplication {
+  @Autowired
+  private TodoDao todoDao;
+
   public static void main(String[] args) {
     SpringApplication.run(SpringBootTodoAppApplication.class, args);
   }
---
+++ src/main/java/com/rizwanmushtaq/dao/TodoDao.java
@@ -0,0 +1,19 @@
+package com.rizwanmushtaq.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TodoDao {
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+
+  public JdbcTemplate getJdbcTemplate() {
+    return jdbcTemplate;
+  }
+
+  public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+}
