5f33b5783bfe45ed1c760f83b5671745b69cd4f0
Rizwan <rizwanmushtaq15@gmail.com>
2025-07-20 20:47:58 +0200

    Main class code cleanup

--- src/main/java/com/rizwanmushtaq/SpringBootTodoAppApplication.java
+++ src/main/java/com/rizwanmushtaq/SpringBootTodoAppApplication.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class SpringBootTodoAppApplication implements CommandLineRunner {
@@ -22,16 +21,5 @@ public class SpringBootTodoAppApplication implements CommandLineRunner {
   @Override
   public void run(String... args) throws Exception {
     System.out.println("Spring Boot Todo App is running...");
-//    Todo todo = new Todo("Learn Spring Boot", "Complete the Spring Boot " +
-//        "tutorial", "In Progress", Date.from(LocalDateTime.now().atZone(java.time.ZoneId.systemDefault()).toInstant()));
-//    logger.info("Creating a new Todo: {}", todo);
-    JdbcTemplate jdbcTemplate = new JdbcTemplate();
-//    todoDao.saveTodo(todo);
-    todoDao.getTodoById(5);
-//    todoDao.deleteTodoById(3);
-//    Todo updatedTodo = new Todo();
-//    updatedTodo.setTitle("Updated Title");
-//    todoDao.updateTodo(updatedTodo, 4);
-    todoDao.getAllTodos();
   }
 }
