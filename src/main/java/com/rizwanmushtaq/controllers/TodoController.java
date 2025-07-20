835c955f1deaa0d96819ebc0dacacd54d2476460
Rizwan <rizwanmushtaq15@gmail.com>
2025-07-20 20:46:45 +0200

    updated TodoController

--- src/main/java/com/rizwanmushtaq/controllers/TodoController.java
+++ src/main/java/com/rizwanmushtaq/controllers/TodoController.java
@@ -1,37 +1,47 @@
 package com.rizwanmushtaq.controllers;
 
+import com.rizwanmushtaq.models.Todo;
+import com.rizwanmushtaq.services.TodoServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class TodoController extends ApiBaseController {
   Logger logger = LoggerFactory.getLogger(TodoController.class);
-//  @GetMapping("/todos")
-//  public ResponseEntity<List<Todo>> getTodos() {
-//    return ResponseEntity.ok({message: "Retrieved all todos"});
-//  }
-//
-//  @GetMapping("/todos/{id}")
-//  public ResponseEntity<Todo> getTodoById(@PathVariable int id) {
-//    return ResponseEntity.ok(TodoService.getTodoById(id));
-//  }
-//
-//  @PostMapping("/todos/create")
-//  public ResponseEntity<Todo> createTodo(@RequestBody Todo todo) {
-//    logger.info("Created Todo {}", todo.toString());
-//    return ResponseEntity.status(HttpStatus.CREATED).body(TodoService.createTodo(todo));
-//  }
-//
-//  @PutMapping("/todos/update/{id}")
-//  public ResponseEntity<Todo> updateTodo(@PathVariable int id,
-//                                         @RequestBody Todo todo) {
-//    return ResponseEntity.ok(TodoService.updateTodo(id, todo));
-//  }
-//
-//  @DeleteMapping("/todos/delete/{id}")
-//  public ResponseEntity<Todo> deleteTodo(@PathVariable int id) {
-//    return ResponseEntity.ok(TodoService.deleteTodo(id));
-//  }
+  @Autowired
+  private TodoServiceImpl todoService;
+
+  @GetMapping("/todos")
+  public ResponseEntity<List<Todo>> getTodos() {
+    return ResponseEntity.ok(todoService.getTodos());
+  }
+
+  @GetMapping("/todos/{id}")
+  public ResponseEntity<Todo> getTodoById(@PathVariable int id) {
+    return ResponseEntity.ok(todoService.getTodoById(id));
+  }
+
+  @PostMapping("/todos/create")
+  public ResponseEntity<Todo> createTodo(@RequestBody Todo todo) {
+    logger.info("Created Todo {}", todo.toString());
+    return ResponseEntity.status(HttpStatus.CREATED).body(todoService.createTodo(todo));
+  }
+
+  @PutMapping("/todos/update/{id}")
+  public ResponseEntity<Todo> updateTodo(@PathVariable int id,
+                                         @RequestBody Todo todo) {
+    return ResponseEntity.ok(todoService.updateTodo(id, todo));
+  }
+
+  @DeleteMapping("/todos/delete/{id}")
+  public ResponseEntity<Todo> deleteTodo(@PathVariable int id) {
+    return ResponseEntity.ok(todoService.deleteTodo(id));
+  }
 }
 
