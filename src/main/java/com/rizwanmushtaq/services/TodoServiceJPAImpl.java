1e28b667d7f44502be2ecc75da52c6cfc6afd9f4
Rizwan <rizwanmushtaq15@gmail.com>
2025-07-22 18:28:50 +0200

    updated TodoServiceImpl

--- src/main/java/com/rizwanmushtaq/services/TodoServiceJPAImpl.java
+++ src/main/java/com/rizwanmushtaq/services/TodoServiceJPAImpl.java
@@ -1,9 +1,11 @@
 package com.rizwanmushtaq.services;
 
 import com.rizwanmushtaq.entities.Todo;
+import com.rizwanmushtaq.exceptions.ResourceNotFoundException;
 import com.rizwanmushtaq.repositories.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,27 +18,38 @@ public class TodoServiceJPAImpl implements TodoService {
 
   @Override
   public List<Todo> getTodos() {
-    List<Todo> todos = todoRepository.findAll();
-    return todos;
+    return todoRepository.findAll();
   }
 
   @Override
   public Todo getTodoById(String id) {
-    return null;
+    return todoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(
+        "Todo not found with id: " + id, HttpStatus.NOT_FOUND));
   }
 
   @Override
   public Todo createTodo(Todo todo) {
-    return null;
+    return todoRepository.save(todo);
   }
 
   @Override
   public Todo updateTodo(String id, Todo todo) {
-    return null;
+    Todo existingTodo = todoRepository.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException(
+            "Todo not found with id: " + id, HttpStatus.NOT_FOUND));
+    existingTodo.setTitle(todo.getTitle());
+    existingTodo.setContent(todo.getContent());
+    existingTodo.setCreationDate(todo.getCreationDate());
+    existingTodo.setStatus(todo.getStatus());
+    return todoRepository.save(existingTodo);
   }
 
   @Override
   public Todo deleteTodo(String id) {
-    return null;
+    Todo existingTodo = todoRepository.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException(
+            "Todo not found with id " + id, HttpStatus.NOT_FOUND));
+    todoRepository.delete(existingTodo);
+    return existingTodo;
   }
 }
