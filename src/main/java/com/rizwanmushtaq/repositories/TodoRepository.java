a5208eb790a0c4967be10a618086f59235f8831c
Rizwan <rizwanmushtaq15@gmail.com>
2025-07-22 18:12:22 +0200

    updated configurations

--- src/main/java/com/rizwanmushtaq/repositories/TodoRepository.java
+++ src/main/java/com/rizwanmushtaq/repositories/TodoRepository.java
@@ -3,5 +3,5 @@ package com.rizwanmushtaq.repositories;
 import com.rizwanmushtaq.entities.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TodoRepository extends JpaRepository<Todo, Integer> {
+public interface TodoRepository extends JpaRepository<Todo, String> {
 }
